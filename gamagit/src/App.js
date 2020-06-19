0bfedf5f3947a1bf1a92c74cea02550db5220da8
santiago <ssantiagotl@gmail.com>
2020-06-18 22:45:54 -0300

    Added 'axios' for requisitions, changed click events and page display

--- gamagit/src/App.js
+++ gamagit/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios'; // add 'axios' for requisitions;
 
-function App() {
+function App(props) { // Function wich receives props and set state;
+const [ usuario, setUsuario ] = useState('');
+
+// function used to get the value "usuario" from imput and search github API for given user repository;
+function handlePesquisa() {
+  axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response));
+
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input className="usuarioInput" placeholder="usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
+      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
+    </>
   );
 }
 
 export default App;
+
