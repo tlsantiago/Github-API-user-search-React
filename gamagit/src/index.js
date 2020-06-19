be61abf8b53da614c0715bec594fcb92f6c00a9e
santiago <ssantiagotl@gmail.com>
2020-06-19 00:01:10 -0300

    Got rid of several unused files created automatically by React

--- gamagit/src/index.js
+++ gamagit/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import Repositories from './Repositories';
+import Repositories from './pages/Repositories';
 
 ReactDOM.render( // "render" function renders JSX html code inside given element;
   <App title="Hello" user="Gama Academy"/>,
   document.getElementById('root') // given element used to render information inside to show on page;
 );
-
-serviceWorker.unregister();
