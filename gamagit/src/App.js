ce2f98a64b4ebf1ee0b3bab4bd8784cc3393005a
santiago <ssantiagotl@gmail.com>
2020-06-18 23:33:32 -0300

    Set 'app.js' as main component, routing to other pages

--- gamagit/src/App.js
+++ gamagit/src/App.js
@@ -1,19 +1,11 @@
 import React, { useState } from 'react';
-import axios from 'axios'; // add 'axios' for requisitions;
 
-function App(props) { // Function wich receives props and set state;
-const [ usuario, setUsuario ] = useState('');
+import Routes from './routes';
 
-// function used to get the value "usuario" from imput and search github API for given user repository;
-function handlePesquisa() {
-  axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response));
+function App() {
 
-}
   return (
-    <>
-      <input className="usuarioInput" placeholder="usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
-      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
-    </>
+    <Routes />
   );
 }
 
--- gamagit/src/routes.js
+++ gamagit/src/routes.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Repositories from './Repositories';
+import Home from './Home';
 
 export default function Routes() {
     return (
-        <Switch>
-            <Route path='/repositories' component={Repositories}/>
-        </Switch>
+
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' exact component={Home} />
+                <Route path='/repositories' component={Repositories} />
+            </Switch>
+        </BrowserRouter>
+
     )
 }
