2204a5d1a3917a7a103151232d45e7a666ab7dd2
santiago <ssantiagotl@gmail.com>
2020-06-21 01:37:20 -0300

    Changed the way that data is displayed on 'repositories' page, wich will now receive each name from localStorage nd verify each one to be displayed trough a 'map' method

--- gamagit/src/pages/Home/index.js
+++ gamagit/src/pages/Home/index.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
 import axios from 'axios'; // add 'axios' for requisitions;
 import * as S from './styled';
+import { useHistory } from 'react-router-dom'; // Imported useHistory, wich controls page changes;
 
 function App(props) { // Function wich receives props and sets;
+  const history = useHistory();
 const [ usuario, setUsuario ] = useState('');
 
 // function used to get the value "usuario" from imput and search github API for given user repository;
 function handlePesquisa() {
   axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
-    const repositories = response.data;
-    const repositoriesName = [];
-    repositories.map((repository) => {
-      repositoriesName.push(repository.name)
+    const repositories = response.data; // Declared a const wich receives full get response data;
+    const repositoriesName = []; // Created a empty array, wich will further receive repositories names;
+    repositories.map((repository) => { // Map method, used to run given function trough each array item;
+      repositoriesName.push(repository.name) // Pushing only the rep. names of each object in array;
     });
-    localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
-});
+    localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName)); // Storing data into local storage;
+    history.push('/repositories'); // Calling history to change to another page after search event;
+  });
 
 }
   return (
--- gamagit/src/pages/Repositories/index.js
+++ gamagit/src/pages/Repositories/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'; // Imported useEffect, wich will monitor variable changes and call a function when it happens. Kinda like eventListener;
 import * as S from './styled';
 
 export default function Repositories() {
+    var [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        let repositoriesName = localStorage.getItem('repositoriesName'); // storing into "repositoriesName" variable the content of "repotoriesName" saved into local storage;
+        repositoriesName = JSON.parse(repositoriesName); // turning this info into object again, for this data is a string in localStorage;
+        setRepositories(repositoriesName); // function called;
+        localStorage.clear();
+    }, []); // Leaving variable empty, so the UseEffect will not monitor a single variable, but execute the function on page load instead.
     return (
         <S.Container>
             <S.Title>Repositórios</S.Title>
             <S.List>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
+                {repositories.map(repository => { // maping through repositories and returning each repository as a 'ListItem'. 
+                    return ( // even though it's not a 'for/while' loop, it works and gets each repository because 'map' will execute the function for each element inside used array;
+                        <S.ListItem>Repositório: {repository}</S.ListItem>
+                    )
+                })}
             </S.List>
         </S.Container>
     )
