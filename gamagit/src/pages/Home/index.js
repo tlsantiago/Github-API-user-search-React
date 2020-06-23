9a681d1a34aeb3a6acb87f46d5fa5fc3039943f9
santiago <ssantiagotl@gmail.com>
2020-06-22 23:31:43 -0300

    Started changing how error message is displayed

--- gamagit/src/pages/Home/index.js
+++ gamagit/src/pages/Home/index.js
@@ -5,26 +5,29 @@ import { useHistory } from 'react-router-dom'; // Imported useHistory, wich cont
 
 function App(props) { // Function wich receives props and sets;
   const history = useHistory();
-const [ usuario, setUsuario ] = useState('');
+  const [usuario, setUsuario] = useState('');
 
-// function used to get the value "usuario" from imput and search github API for given user repository;
-function handlePesquisa() {
-  axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
-    const repositories = response.data; // Declared a const wich receives full get response data;
-    const repositoriesName = []; // Created a empty array, wich will further receive repositories names;
-    repositories.map((repository) => { // Map method, used to run given function trough each array item;
-      repositoriesName.push(repository.name) // Pushing only the rep. names of each object in array;
+  // function used to get the value "usuario" from imput and search github API for given user repository;
+  function handlePesquisa() {
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+      const repositories = response.data; // Declared a const wich receives full get response data;
+      const repositoriesName = []; // Created a empty array, wich will further receive repositories names;
+      repositories.map((repository) => { // Map method, used to run given function trough each array item;
+        repositoriesName.push(repository.name) // Pushing only the rep. names of each object in array;
+      });
+      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName)); // Storing data into local storage;
+      history.push('/repositories'); // Calling history to change to another page after search event;
     });
-    localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName)); // Storing data into local storage;
-    history.push('/repositories'); // Calling history to change to another page after search event;
-  });
 
-}
+  }
   return (
-    <S.Container>
-      <S.Input className="usuarioInput" placeholder="usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
-      <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
-    </S.Container>
+    <S.HomeContainer>
+      <S.Content>
+        <S.Input className="usuarioInput" placeholder="usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
+        <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+        <S.ErrorMsg>Ocorreu um erro, tente novamente</S.ErrorMsg>
+      </S.Content>
+    </S.HomeContainer>
   );
 }
 
